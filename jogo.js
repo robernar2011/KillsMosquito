8afd6e13f2ddc17744b8d37fee7c5ec4294eb4a4
robernar2011 <robernar2011@gmail.com>
2022-01-26 16:14:09 -0300

    Created randomic positions to mosquito

--- jogo.js
+++ jogo.js
@@ -1,3 +1,5 @@
+//Ajustando o palco do jogo
+
 var altura = 0
 var largura = 0
 
@@ -5,7 +7,32 @@ function ajustaTamanhoPalcoJogo() {
 	altura = window.innerHeight
 	largura = window.innerWidth
 
-	console.log(altura, largura)
+	console.log(largura, altura)
 }
 
 ajustaTamanhoPalcoJogo()
+
+//Posições randômicas para o mosquito
+
+function posicaoRandomica(){
+	var posicaoX = Math.floor(Math.random() * largura) - 90
+	var posicaoY = Math.floor(Math.random() * altura) - 90
+
+	//Se posição x ou y for menor que zero, recebe zero, senão recebe ela mesma
+	posicaoX = posicaoX < 0 ? 0 : posicaoX
+	posicaoY = posicaoY < 0 ? 0 : posicaoY
+
+	console.log(posicaoX, posicaoY)
+
+	//Criar o elemento html de forma dinâmica
+
+	var mosquito = document.createElement('img')
+	mosquito.src = 'imagens/mosquito.png'
+	mosquito.className = 'mosquito1'
+	mosquito.style.left = posicaoX + 'px' //pixel
+	mosquito.style.top = posicaoY + 'px'
+	mosquito.style.position = 'absolute'
+
+	document.body.appendChild(mosquito)
+}
+
